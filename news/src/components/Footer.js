956b85a4e6075083abba79c2573d879af67f4ff0
Juan Diego Salazar <salazar.jd@gmail.com>
2023-07-01 20:19:33 -0500

    app news created

---
+++ news/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Footer({autor = ''}) {
+  return (
+    <footer className='blog-footer'>
+        <p>
+            {autor}
+        </p>
+    </footer>
+  )
+}
